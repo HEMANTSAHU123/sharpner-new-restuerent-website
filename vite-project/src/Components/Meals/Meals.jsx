4f113a661449ef42c23504583ea2cc9a93368fec
HEMANTSAHU123 <hs1282542@gmail.com>
2025-02-19 16:44:15 +0530

    meals is added

---
+++ vite-project/src/Components/Meals/Meals.jsx
@@ -0,0 +1,12 @@
+import React,{Fragment} from 'react'
+import Available from './Available'
+import MealsSummary from './MealsSummary'
+const Meals = () => {
+  return <Fragment>
+      <Available/>
+      <MealsSummary/>
+    </Fragment>
+  
+}
+
+export default Meals
---
+++ vite-project/src/Components/Meals/MealsSummary.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './MealsSummary.css'
+const MealsSummary = () => {
+  return (
+    <section className='summary'>
+   
+      <h2>Delicious food ,delivered to you </h2>
+      <p>choose ur favourtite meal from our board selection of available meal and enjoy a delicious lunch or food</p>
+      <p>all our meal are cooked with heigh gradient just in times and ofcourse by experienced chefs</p>
+    </section>
+  )
+}
+
+export default MealsSummary;
