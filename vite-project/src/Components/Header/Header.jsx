391914d4d1b68c2f702a5f6dded9d942c0a7c6da
HEMANTSAHU123 <hs1282542@gmail.com>
2025-02-19 14:42:10 +0530

    completed cart

--- vite-project/src/Components/Header/Header.jsx
+++ vite-project/src/Components/Header/Header.jsx
@@ -1,11 +1,13 @@
 import React,{Fragment} from 'react'
 import Mealimage from '../../assets/meals.jpg'
+import HeaderCartButton from './HeaderCartButton'
 import './Header.css'
 const Header = () => {
   return (
     <Fragment>
       <header className='header'>
         <h1>React Meal</h1>
+        <HeaderCartButton/>
         <button>Cart</button>
       </header>
       <div className='main-image'>
